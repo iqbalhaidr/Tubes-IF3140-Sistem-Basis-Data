30b2d140fd0451ec6012a7345e24a51a8ed03907
Benedict Presley <ben@DESKTOP-QA4PTOQ.localdomain>
2025-11-13 20:00:02 +0700

    fix: merged error

--- include/failure_recovery.h
+++ include/failure_recovery.h
@@ -2,6 +2,7 @@
 #include "types.h"
 #include <vector>
 #include <ctime>
+#include <string>
 
 namespace mdbms::fr {
 
@@ -10,6 +11,13 @@ public:
     void write_log(const ExecutionResult& info);
     void save_checkpoint();
     void recover(const RecoverCriteria& criteria);
+
+private:
+    std::vector<LogEntry> log_buffer;
+    std::vector<CheckpointInfo> checkpoints;
+    std::string log_file_path;
+    int next_log_id;
+    int next_checkpoint_id;
 };
 
 } // namespace mdbms::fr
